f32f6510554f01ec93254b3fd32504ca1b135b31
nerun <danieldiasr@gmail.com>
2025-06-28 14:32:55 -0300

    restored spanish translator name

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -3,6 +3,7 @@
  * Spanish language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Jos Antonio Fuentes Santiago <joanfusan@hotmail.com>
  * @author     Daniel "Nerun" Rodrigues <danieldiasr@gmail.com>
  */
  
