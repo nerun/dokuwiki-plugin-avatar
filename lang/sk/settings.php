861f041973d28cbeb053212a01b796d16bbd7713
nerun <danieldiasr@gmail.com>
2026-09-19 09:25:31 -0300

    Translation update (sk)

--- lang/sk/settings.php
+++ lang/sk/settings.php
@@ -3,13 +3,13 @@
  * Slovak language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Pirhala Marek <mpirhala@genesispo.sk>
  * @author     Daniel "Nerun" Rodrigues <danieldiasr@gmail.com>
+ * @assistant  ChatGPT (GPT-5.6 Sol)
  */
  
 // for the configuration manager
-$lang['namespace'] = 'Menný priestor pre lokálne avatary';
-$lang['size']      = 'Predvolená veľkosť avatara';
-$lang['rating']    = 'Minimálne hodnotenie pre gravatary';
-$lang['default']   = 'Typ predvolených gravatary';
-
+$lang['namespace']        = 'Predvolený menný priestor pre lokálne avatary';
+$lang['size']             = 'Predvolená veľkosť avatara v pixeloch';
+$lang['rating']           = 'Maximálne povolené hodnotenie obsahu pre gravatary';
+$lang['gravatar_default'] = 'Predvolený generátor obrázka, keď k e-mailovej adrese neexistuje zodpovedajúci obrázok Gravatar';
+$lang['local_default']    = 'Predvolený generátor obrázka, keď používateľ nemá zodpovedajúci lokálny avatar';
